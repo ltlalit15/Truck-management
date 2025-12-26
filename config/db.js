b3ab51aa4b25d5bdbd6fc06380dfa5b009b9a6aa
ltlalit15 <ltlalit15@gmail.com>
2025-12-26 13:28:35 +0530

    Mr Robot

--- config/db.js
+++ config/db.js
@@ -13,7 +13,7 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
-  db_port: process.env.DB_PORT,
+  port: process.env.DB_PORT,
   connectionLimit: 10000,
   queueLimit: 0,
   enableKeepAlive: true,
